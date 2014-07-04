7a34640a990844c54f71c573c81850b4b1d0d702
Kai Lindholm <megantti@gmail.com>
2014-07-04 08:17:40 +0300

    Add a simple world.

--- Terrain.hs
+++ Terrain.hs
@@ -1,8 +1,44 @@
+{-# LANGUAGE TemplateHaskell, Rank2Types #-}
 module Terrain where
 
-data Terrain = Terrain { 
-      terranWidth    :: Int
-    , terrainHeight  :: Int
+import qualified Data.Vector as V
+import Control.Applicative ((<$>))
+import Control.Lens
+import Control.Lens.TH
+
+data TileType = 
+	  TileEmpty
+	| TileGround
+	deriving Enum
+
+data Tile = Tile {
+	  _tileCreatures :: [Int]
+	, _tileType	    :: TileType
 }
 
-data Tile = Tile 
+
+data Terrain = Terrain { 
+      _terrainWidth   :: Int
+    , _terrainHeight  :: Int
+	, _terrainDepth	  :: Int
+	, _terrainTiles   :: V.Vector Tile
+}
+
+
+makeLenses ''Tile
+makeLenses ''Terrain
+
+indexTerrain terrain (x, y, z) = x + y * w + z * w * h
+  where
+	w = view terrainWidth terrain
+	h = view terrainHeight terrain
+
+getTerrainTile :: Terrain -> (Int, Int, Int) -> Tile
+getTerrainTile terrain pos = (view terrainTiles terrain) V.! (indexTerrain terrain pos)
+
+setTerrainTile :: Terrain -> (Int, Int, Int) -> Tile -> Terrain
+setTerrainTile terrain pos tile = terrain & terrainTiles %~ (V.// [(indexTerrain terrain pos, tile)]) 
+
+terrainTile :: (Int, Int, Int) -> Lens' Terrain Tile
+terrainTile pos f terrain = setTerrainTile terrain pos <$> f (getTerrainTile terrain pos)
+
--- World.hs
+++ World.hs
@@ -1,2 +1,80 @@
+{-# LANGUAGE TemplateHaskell, Rank2Types #-}
 module World where
 
+import qualified Data.IntMap as IM
+import qualified Data.Vector as V
+import Control.Lens
+import Control.Lens.TH
+import Control.Monad.State
+import Control.Applicative ((<$>))
+
+import Terrain
+
+type CreatureId = Int
+
+data CreatureType = 
+	CreatureNefle
+  deriving Enum
+
+data Creature = Creature {
+	  _creatureType :: CreatureType
+	, _creatureId   :: CreatureId
+	, _creaturePos  :: (Int, Int, Int)
+	, _creatureAct  :: CreatureId -> State World ()
+}
+
+data World = World {
+	  _worldTerrain   :: Terrain
+	, _worldCreatures :: IM.IntMap Creature
+	, _worldMaxId	  :: Int
+}
+
+makeLenses ''World
+makeLenses ''Creature
+
+creatureById :: CreatureId -> Lens' World Creature
+creatureById i = worldCreatures . imLens
+  where
+	imLens f k = (\v -> IM.insert i v k) <$> f (k IM.! i)
+
+stepWorld :: State World ()
+stepWorld = do
+	creatures <- IM.toList <$> use worldCreatures
+	mapM_ (\(i, creature) -> (creature ^. creatureAct) i) creatures
+
+simpleTerrain :: Terrain
+simpleTerrain = Terrain {
+	  _terrainWidth = 10
+	, _terrainHeight = 10
+	, _terrainDepth = 1
+	, _terrainTiles = V.replicate 100 tile
+}
+	where
+		tile = Tile {
+			  _tileCreatures = []
+			, _tileType = TileEmpty
+		}
+
+simpleCreature :: Creature 
+simpleCreature = Creature {
+	  _creatureType = CreatureNefle
+	, _creatureId = 1
+	, _creaturePos = (5, 5, 0)
+	, _creatureAct = const $ return ()
+}
+
+addCreature :: Creature -> World -> World
+addCreature creature world =
+	world & worldMaxId +~ 1
+		  & worldCreatures %~ IM.insert newId newCreature
+		  & worldTerrain . terrainTile (creature ^. creaturePos) . tileCreatures %~ (newId :)
+  where
+	newId = world ^. worldMaxId + 1 
+	newCreature = creature & creatureId .~ newId
+
+simpleWorld :: World
+simpleWorld = addCreature simpleCreature $ World {
+	  _worldTerrain = simpleTerrain
+	, _worldCreatures = IM.empty
+	, _worldMaxId = 0
+}
